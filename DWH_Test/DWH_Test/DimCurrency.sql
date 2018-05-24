f1598355bac0f07ce7e012dc951d72e8f1fd3e6e
Nat <sqlnat@gmail.com>
2018-05-24 12:19:22 +0100

    New version / Currency ID column rename

--- DWH_Test/DWH_Test/DimCurrency.sql
+++ DWH_Test/DWH_Test/DimCurrency.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[DimCurrency](
-	[CurrencyKey] [int] IDENTITY(1,1) NOT NULL,
+	[CurrencyID] [int] IDENTITY(1,1) NOT NULL,
 	[CurrencyAlternateKey] [nchar](3) NOT NULL,
 	[CurrencyName] [nvarchar](50) NOT NULL,
  CONSTRAINT [PK_DimCurrency_CurrencyKey] PRIMARY KEY CLUSTERED 
 (
-	[CurrencyKey] ASC
+	[CurrencyID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
